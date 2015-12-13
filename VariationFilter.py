8da2e5211b7d4a77ead38eda1f7dcf11b2bd48ab
gl lin <linguoliang1313@gmail.com>
2015-12-13 20:50:04 +0800

    update

--- VariationFilter.py
+++ VariationFilter.py
@@ -1,4 +1,33 @@
 __author__ = 'Guoliang Lin'
-Version='1.0.0'
-# coding utf-8
-#
+Softwarename = 'splitGFF'
+version = '1.0.1'
+bugfixs = ''
+import sys, getopt
+import time
+
+
+def trim(y):
+    y = y.replace("[", '')
+    y = y.replace(']', '')
+    y = y.replace("',", '\t')
+    y = y.replace("'", '')
+    y = y.replace('\\n', '')
+    y = y.replace(' ', '')
+    # y=y.replace(',','')
+    y = y.strip()
+    y = y + '\n'
+    return y
+
+
+print('%s software version is %s' % (Softwarename, version))
+print(bugfixs)
+print('starts at :' + time.strftime('%Y-%m-%d %H:%M:%S'))
+
+opts, args = getopt.getopt(sys.argv[1:], 'i:h', ['inputfile=', 'help'])
+InputFileName = ''
+for o, a in opts:
+    if o in ['-i', '--inputfile']:
+        fpkm = a
+    elif o in ['-h', '--help']:
+        help = True
+with open(InputFileName, 'w') as InputFile:
